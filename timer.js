23f6057fe1b358157b2261dc57ac1a00b2390e58
{magesharun} <{magesharun2016@gmail.com}>
2023-12-24 12:16:53 +0530

    first commit

---
+++ timer.js
@@ -0,0 +1,47 @@
+const days=document.querySelector("#days");
+const hours=document.querySelector("#hours");
+const minutes=document.querySelector("#minutes");
+const seconds=document.querySelector("#seconds");
+
+
+function updateTime()
+{
+    const currentYear = new Date().getFullYear();
+
+// Fix the string for the Date constructor
+const newYear = new Date(`January 1 ${currentYear + 1} 00:00:00`);
+
+console.log(newYear);
+
+const currentDate = new Date();
+console.log(currentDate);
+
+const diff = newYear - currentDate;
+const d = Math.floor(diff / (1000 * 60 * 60 * 24));
+
+
+const h = Math.floor((diff / (1000 * 60 * 60)) % 24);
+
+
+const m = Math.floor((diff / (1000 * 60)) % 60);
+
+
+const s = Math.floor((diff / 1000) % 60);
+
+days.innerHTML=d<10?"0"+d:d;
+hours.innerHTML=h<10?"0"+h:h;
+minutes.innerHTML=m<10?"0"+m:m;
+seconds.innerHTML=s<10?"0"+s:s;
+
+
+}
+setInterval(updateTime,1000);
+
+/*
+1000ms=1s
+60s=1m
+60m=1hr
+24hrs=1day
+31days=1month
+12months=1year
+*/
